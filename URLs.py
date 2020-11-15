2b5dcb4f9782ebf4a01c467f78078726f0c43f76
hoanghai0598 <53805535+hoanghai0598@users.noreply.github.com>
2020-11-15 13:37:17 +0700

    Update URLs.py

--- URLs.py
+++ URLs.py
@@ -86,7 +86,7 @@ model = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 print(confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
-#print(classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
+print(classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
 
 print('------------ALLDONE-------------')
 
